7892a94d320cdde5197e5eda0756bf264b75413c
Curtis Robinson <38477322+curt60@users.noreply.github.com>
2018-06-26 14:08:52 -0700

    updated audio, including random attack sounds, and character formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,109 @@
+//define single game object at the global scope that includes all custom vars and functions
+
 //define vars
-var hero = "";
-var enemy = "";
+var hero = ""; //currently selected hero
+var enemy = ""; //currently selected enemy
 var gameState = "new";
+var audioAttackSounds = [];
+var audioAttackIndex = 0;
 
 //create audio objects
-var audioGameTheme = new Sound("assets/audio/theme.mp3", .02);
-var audioAttack = new Sound("assets/audio/blaster.mp3", 1);
+var audioGameTheme = new Sound("assets/audio/theme-1.mp3", .02);
+var audioBattleStart = new Sound("assets/audio/battle-start.wav", 1);
+var audioGameOver = new Sound("assets/audio/game-over.mp3", 1);
+for (let i = 0; i < 12; i++) {
+    audioAttackSounds[i] = new Sound("assets/audio/attack-" + i + ".wav", 1);
+}
 
 //create character objects
-var yoda = new Character("yoda", 100, 20, 15, "assets/audio/yodalaughing.mp3");
-var vadar = new Character("vadar", 100, 20, 15, "assets/audio/darth-quote-1.mp3");
+var yoda = new Character("yoda", 100, 20, 15, "assets/audio/yoda.mp3");
+var vadar = new Character("vadar", 100, 20, 15, "assets/audio/vadar.mp3");
+var maul = new Character("maul", 100, 20, 15, "assets/audio/maul.wav");
+var luke = new Character("luke", 100, 20, 15, "assets/audio/luke.mp3");
+var leia = new Character("leia", 100, 20, 15, "assets/audio/leia.mp3");
+var rey = new Character("rey", 100, 20, 15, "assets/audio/rey.mp3");
 
 //event listeners for keyboard
 document.onkeyup = function(event) {
+    //start theme song after pressing any key (can't start before user interaction due to chrome policy)
     audioGameTheme.play();
 }
 
 //event listeners for character clicks
 $(".character").click(function() {
-    //if character in available section && hero not set
-        //move the character to hero spot
-        //speak
-    //if character in available section && hero set
-    
+    //move character to hero or enemy section
+    if (!hero) {
+        //remove hero selection instructions
+        $(".heroChar > .instructions")[0].style.display = "none";
+        //move selected character to hero section
+        window[this.id].move(".heroChar");
+        //display enemy selection instructions
+        $(".enemyChar > .instructions")[0].style.display = "initial";
+        //change border of all characters
+        let charElements = $(".character")
+        for (i = 0; i < charElements.length; i++) {
+            if (charElements[i].id === hero) {
+                charElements[i].style.border = "1px solid #007bff";
+            }
+            else {
+                charElements[i].style.border = "1px solid red"; 
+            }
+        }
+    }
+    else if (!enemy && this.id != hero) {
+        //remove enemy selection instructions
+        $(".enemyChar > .instructions")[0].style.display = "none";
+        //move selected character to enemy section
+        window[this.id].move(".enemyChar");
+        //play battle start sound
+        window.setTimeout(function() {audioBattleStart.play();}, 3000);
+    }
     window[this.id].speak();
     console.log("Clicked: " + this.id);
 });
 
 //event listener for attach button
 $("button").click( function() {
-    audioAttack.play();
+    //stop current sound if stil playing
+    audioAttackSounds[audioAttackIndex].stop();
+    audioAttackSounds[audioAttackIndex].sound.currentTime = 0;
+    //play new random attack sound
+    audioAttackIndex = Math.floor(Math.random() * 12);
+    audioAttackSounds[audioAttackIndex].play();
+    console.log("attack-" + audioAttackIndex + ".wav")
 });
 
 
 //display starting instructions
+    //"press any key to begin"
 
 //keyboard input
     //play theme song
-    //remove instructions
-    //initialize game(?)
+    //remove initial instruction dialog
 
-//select hero
-    //set hero var
-    //character speak
-    //character move
+//on character click:
+    //if !hero
+        //select hero
+            //display stats
+    //else if !enemy
+        //select enemy
+            //display stats
 
-//select enemy
-    //set enemy var
-    //character speak
-    //character move
-    //delay ...light saber on
-
-//attack
-    //calculate damage for both chars and update stats
-    //if one character dead
+//on attack click
+    //calculate damage for both chars
+    //update stats
+    //if hero dead
+        //game over sequence
+    //if enemy dead
         //remove character
-        //wait for 'select enemy'
+        //play: turn off saber
+        //reset enemy var
+        //add instructions for selecting next enemy
 
     
 
-//end of battle
-    //turn off saber
 
-
-//audio object constructor
+//sound object constructor (I didn't realize that a built-in constructor exists)
 function Sound(src, vol) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
@@ -87,12 +127,36 @@ function Character(name, health, attack, counter, audioUrl) {
     this.healthPoints = health;
     this.attackPoints = attack;
     this.counterPoints = counter;
+    //set character audio w/ volume 100%
     this.audioTheme = new Sound(audioUrl, 1);
     this.speak = function() {
         this.audioTheme.play();
+    };
+    //check if character died
     this.isDead = function() {
         if (this.healthPoints < 0) true;
         else false;
-    }
+    };
+    //character move sequence (with slow transition)
+    this.move = function(destination) {
+        //store character HTML element as variable
+        var charElement = $("#" + name);
+        //set hero or enemy var
+        if (destination === ".heroChar") hero = name;
+        else if (destination === ".enemyChar") enemy = name;
+        //add slow transition to move
+        charElement[0].style.opacity = 0;
+        setTimeout(function() {$(destination).append(charElement);}, 800);
+        setTimeout(function() {charElement[0].style.opacity = 1;}, 1000);
     }
 }
+
+//remove character from enemy section (used after defeated)
+function removeEnemy() {
+    var parentElement = $(".enemyChar");
+    var charElement = $("#" + enemy);
+    //add slow transition
+    charElement[0].style.opacity = 0;
+    setTimeout(function() {parentElement[0].removeChild(charElement[0])}, 800);
+}
+
