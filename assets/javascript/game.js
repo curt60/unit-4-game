05d52f90746b9311c0410ec8b1436189efbfb684
Curtis Robinson <38477322+curt60@users.noreply.github.com>
2018-06-25 21:36:13 -0700

    updated css with flexbox layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,21 +4,63 @@ var enemy = "";
 var gameState = "new";
 
 //create audio objects
-var audioGameTheme = new Sound("assets/audio/theme-2.mp3", .02);
-var audioYodaTheme = new Sound("assets/audio/yodalaughing.mp3", 1);
-var audioVadarTheme = new Sound("assets/audio/darth-quote-1.mp3", 1);
+var audioGameTheme = new Sound("assets/audio/theme.mp3", .02);
+var audioAttack = new Sound("assets/audio/blaster.mp3", 1);
 
 //create character objects
-var yoda = new Character("yoda", 100, 20, 15, audioYodaTheme);
-var vadar = new Character("vadar", 100, 20, 15, audioVadarTheme);
+var yoda = new Character("yoda", 100, 20, 15, "assets/audio/yodalaughing.mp3");
+var vadar = new Character("vadar", 100, 20, 15, "assets/audio/darth-quote-1.mp3");
 
+//event listeners for keyboard
+document.onkeyup = function(event) {
+    audioGameTheme.play();
+}
 
 //event listeners for character clicks
 $(".character").click(function() {
+    //if character in available section && hero not set
+        //move the character to hero spot
+        //speak
+    //if character in available section && hero set
+    
     window[this.id].speak();
     console.log("Clicked: " + this.id);
 });
 
+//event listener for attach button
+$("button").click( function() {
+    audioAttack.play();
+});
+
+
+//display starting instructions
+
+//keyboard input
+    //play theme song
+    //remove instructions
+    //initialize game(?)
+
+//select hero
+    //set hero var
+    //character speak
+    //character move
+
+//select enemy
+    //set enemy var
+    //character speak
+    //character move
+    //delay ...light saber on
+
+//attack
+    //calculate damage for both chars and update stats
+    //if one character dead
+        //remove character
+        //wait for 'select enemy'
+
+    
+
+//end of battle
+    //turn off saber
 
 
 //audio object constructor
@@ -28,7 +70,7 @@ function Sound(src, vol) {
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
-    this.sound.volume = vol;
+    this.sound.volume = (vol || 1);
     document.body.appendChild(this.sound);
     this.play = function(){
         this.sound.play();
@@ -40,13 +82,17 @@ function Sound(src, vol) {
 
 
 //character object constructor
-function Character(name, health, attack, counter, audioTheme) {
+function Character(name, health, attack, counter, audioUrl) {
     this.name = name;
     this.healthPoints = health;
     this.attackPoints = attack;
     this.counterPoints = counter;
-    this.audioTheme = audioTheme;
+    this.audioTheme = new Sound(audioUrl, 1);
     this.speak = function() {
         this.audioTheme.play();
+    this.isDead = function() {
+        if (this.healthPoints < 0) true;
+        else false;
+    }
     }
 }
